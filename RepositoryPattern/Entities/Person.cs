9a4678b2e8cb89dec630db8403a17850794edb0d
sstk14 <mail>
2019-10-22 18:59:49 +0900

    プロジェクト ファイルを追加します。

---
+++ RepositoryPattern/Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Entities
+{
+    public class Person
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastAndMiddleName { get; set; }
+    }
+}
---
+++ RepositoryPattern/Interfaces/IRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Interfaces
+{
+    /// <summary>
+    /// データのアクセス処理を抽象化するためのインターフェース
+    /// </summary>
+    public interface IRepository<T>
+    {
+        /// <summary>
+        /// 指定IDを持つオブジェクト取得
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        T Get(int id);
+
+        /// <summary>
+        /// 全データ取得
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> GetAll();
+
+        /// <summary>
+        /// 指定データの追加処理
+        /// </summary>
+        /// <param name="item">追加対象オブジェクト</param>
+        void Add(T item);
+
+        /// <summary>
+        /// 指定データの更新処理
+        /// </summary>
+        /// <param name="item">更新対象オブジェクト</param>
+        void Update(T item);
+
+        /// <summary>
+        /// 指定データの削除処理
+        /// </summary>
+        /// <param name="item">削対象オブジェクト</param>
+        void Delete(T item);
+    }
+}
